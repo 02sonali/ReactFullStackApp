08f76e7c001195c61b3c2f665a56506bfd2b5384
Sonali Gupta <sonali.gupta01@nagarro.com>
2023-07-24 14:04:31 +0530

    setup fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
@@ -7,16 +8,25 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: "index.html", // to import index.html file inside index.js
+        }),
+      ],
     resolve: {
         extensions: ['.js', '.jsx'] //list of extensions that you want webpack to process
     },
     devServer: {
-        historyApiFallback: true // required for react-router to work
+        historyApiFallback: true, // required for react-router to work
+        port: 3030, // you can change the port
     },
     module: {  //describes how we want our app to compile
         rules: [{
             test: /\.jsx?$/, //determines which files should be compiled (any file with .js or .jsx extension here)
-            loader: 'babel-loader'
+            exclude: /node_modules/, // excluding the node_modules folder
+            use: {
+            loader: "babel-loader",
+            }
         }]
     }
 }
